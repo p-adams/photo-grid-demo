7904d71a2d02c557083171400669baf82834a328
Paul Adams <ghusiyy@gmail.com>
2022-08-17 23:03:31 -0700

    // TODO: implement card flip

--- src/photoCard.ts
+++ src/photoCard.ts
@@ -1,11 +1,25 @@
 import { Photo } from "./api";
 
+function createEl(tagName: string, options: { name: string; value: string }[]) {
+  const el = document.createElement(tagName);
+  for (const { name, value } of options) {
+    el.setAttribute(name, value);
+  }
+  return el;
+}
+
 export function setupPhotoCard(photo: Photo): HTMLElement {
-  const card = document.createElement("figure");
-  card.setAttribute("class", "card-wrapper");
-  const img = document.createElement("img");
-  img.setAttribute("src", photo.url);
-  img.setAttribute("alt", photo.title);
+  const card = createEl("figure", [{ name: "class", value: "card-wrapper" }]);
+
+  const img = createEl("img", [
+    { name: "src", value: photo.url },
+    { name: "alt", value: photo.title },
+  ]);
+
   card.appendChild(img);
+  // TODO: implement card flip
+  function handleFlipCard() {}
+  card.addEventListener("click", handleFlipCard);
+
   return card;
 }
