e74c0cf4369709de84991f8c374738d4d1f1f432
Paul Adams <ghusiyy@gmail.com>
2022-08-17 22:51:04 -0700

    render photo in card

--- src/photoCard.ts
+++ src/photoCard.ts
@@ -1,3 +1,11 @@
-import { Photo } from "./photoGrid";
+import { Photo } from "./api";
 
-export function setupPhotoCard(el: HTMLDivElement, photo: Photo) {}
+export function setupPhotoCard(photo: Photo): HTMLElement {
+  const card = document.createElement("figure");
+  card.setAttribute("class", "card-wrapper");
+  const img = document.createElement("img");
+  img.setAttribute("src", photo.url);
+  img.setAttribute("alt", photo.title);
+  card.appendChild(img);
+  return card;
+}
--- src/photoGrid.ts
+++ src/photoGrid.ts
@@ -1,14 +1,10 @@
 import { getPhotos } from "./api";
+import { setupPhotoCard } from "./photoCard";
 
 export async function setupPhotoGrid(el: HTMLDivElement) {
   for (const photo of await getPhotos()) {
     // TODO: use photo card element
-    const imgWrapper = document.createElement("figure");
-    imgWrapper.setAttribute("class", "img-wrapper");
-    const img = document.createElement("img");
-    img.setAttribute("src", photo.url);
-    img.setAttribute("alt", photo.title);
-    imgWrapper.appendChild(img);
-    el!.appendChild(imgWrapper);
+    const photoCard = setupPhotoCard(photo);
+    el!.appendChild(photoCard);
   }
 }
