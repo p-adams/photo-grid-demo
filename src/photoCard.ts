4e7c446539b6bdae40899a5e401daaf24ab416a4
Paul Adams <ghusiyy@gmail.com>
2022-08-21 00:39:40 -0700

    remove helper function

--- src/photoCard.ts
+++ src/photoCard.ts
@@ -1,26 +1,19 @@
 import { Photo } from "./api";
 
-function createEl(tagName: string, options: { name: string; value: string }[]) {
-  const el = document.createElement(tagName);
-  for (const { name, value } of options) {
-    el.setAttribute(name, value);
-  }
-  return el;
-}
-
 export function setupPhotoCard(photo: Photo): HTMLElement {
-  const cardContainer = createEl("div", [
-    { name: "class", value: "card-container" },
-  ]);
-  cardContainer.innerHTML = `<div class="card-wrapper" ontouchstart="this.classList.toggle('hover');">
-    <div class="front">
-       <img src=${photo.thumbnailUrl}/>
-       <h3>${photo.title}</h3>
-    </div>
-    <div class="back">
-        <img src=${photo.url}/>
-    </div>
-  </div>`;
+  const cardContainer = document.createElement("div");
+  cardContainer.setAttribute("class", "card-container");
+
+  cardContainer.innerHTML = `
+    <div class="card-wrapper" ontouchstart="this.classList.toggle('hover');">
+      <div class="front">
+        <img src=${photo.thumbnailUrl}/>
+        <h3>${photo.title}</h3>
+      </div>
+      <div class="back">
+          <img src=${photo.url}/>
+      </div>
+    </div>`;
 
   return cardContainer;
 }
--- src/photoGrid.ts
+++ src/photoGrid.ts
@@ -3,7 +3,6 @@ import { setupPhotoCard } from "./photoCard";
 
 export async function setupPhotoGrid(el: HTMLDivElement) {
   for (const photo of await getPhotos()) {
-    // TODO: use photo card element
     const photoCard = setupPhotoCard(photo);
     el!.appendChild(photoCard);
   }
